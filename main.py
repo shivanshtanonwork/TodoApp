5d7d25f03029e1449a0565cd199fc6cd18298173
shivanshtanonwork <shivanshtandonwork@gmail.com>
2026-05-14 13:18:08 +0530

    Get all todo and todo by id

--- main.py
+++ main.py
@@ -1,11 +1,33 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends, HTTPException, status, Path
+from typing import Annotated
+from sqlalchemy.orm import Session
 import models
-from database import engine
+from models import Todos
+from database import engine, sessionLocal
 
 app = FastAPI()
 
+# this will create the tables in the database if they don't exist
 models.Base.metadata.create_all(bind=engine)
 
-@app.get("/")
-async def home():
-    return {"message": "Hello, Shivansh!"}
+
+def get_db():
+    db = sessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+db_dependency = Annotated[Session, Depends(get_db)]
+
+@app.get("/", status_code=status.HTTP_200_OK)
+async def read_all(db: db_dependency):
+    return db.query(Todos).all()
+
+@app.get("/todo/{todo_id}", status_code=status.HTTP_200_OK)
+async def read_todo(db: db_dependency, todo_id: int = Path(gt=0)):
+    todo_model = db.query(Todos).filter(Todos.id == todo_id).first()
+    if todo_model is not None:
+        return todo_model
+    raise HTTPException(status_code=404, detail="Todo not found")
+
